fbf4287b0e97618f780d8520f7268ca1bccb979a
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2023-08-26 20:55:02 +0700

    chorus: change seed user

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -21,13 +21,24 @@ class UsersSeeder extends Seeder {
         DB::table('users')->truncate();
         Schema::enableForeignKeyConstraints();
 
-        $user = User::create([
-            'email' => 'admin@hydra.project',
-            'name' => 'Hydra Admin',
-        ]);
-        UserRole::create([
-            'user_id' => $user->id,
-            'role_id' => Role::where('slug', 'admin')->first()->id,
-        ]);
+        $data = [
+            ['email' => 'admin@hydra.project','name' => 'Hydra Admin'],
+            ['email' => 'c14210073@john.petra.ac.id','name' => 'ceje'],
+            ['email' => 'c14210025@john.petra.ac.id','name' => 'Darrel'],
+            ['email' => 'c14210017@john.petra.ac.id','name' => 'Nico'],
+            ['email' => 'c14210206@john.petra.ac.id','name' => 'Leo'],
+            ['email' => 'c14210099@john.petra.ac.id','name' => 'Nichgun']
+        ];
+        foreach ($data as $userData) {
+            $user = User::create([
+                'email' => $userData['email'],
+                'name' => $userData['name'],
+            ]);
+            UserRole::create([
+                'user_id' => $user->id,
+                'role_id' => Role::where('slug', 'admin')->first()->id,
+            ]);
+        }
+
     }
 }
