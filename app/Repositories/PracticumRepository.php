68ea2883525e4f061ee9070efda450970f88266b
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2023-10-28 15:53:00 +0700

    feat: modify the getById for practicum

--- app/Repositories/PracticumRepository.php
+++ app/Repositories/PracticumRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Models\Practicum;
 use App\Repositories\BaseRepository;
+use Illuminate\Support\Facades\DB;
 
 class PracticumRepository extends BaseRepository
 {
@@ -17,4 +18,17 @@ class PracticumRepository extends BaseRepository
         OR
         Override existing repository here...
     */
+
+    public function getById($id){
+        return $this->model->with([
+            "subject",
+            "room",
+            "assistantPracticum:id,assistant_id,practicum_id", // Include practicum_id
+            "assistantPracticum.assistant:user_id", // Include user_id
+            "assistantPracticum.assistant.user:id,name,email",
+            "studentPracticum:id,student_id,practicum_id", // Include practicum_id
+            "studentPracticum.assistant:id,user_id", // Include assistant_id and user_id
+            "studentPracticum.student.user:id,name,email"
+        ])->findOrFail($id);
+    }
 }
