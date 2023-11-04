3a6576db0b3a8288874c976fec89d22a1d115dab
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2023-11-04 21:57:36 +0700

    feat: bulk insert user & student

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\BaseController;
+use App\Models\Role;
 use App\Models\Student;
+use App\Models\User;
+use App\Models\UserRole;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends BaseController
 {
+    private $user;
     public function __construct(Student $model)
     {
         parent::__construct($model);
+        $this->user = new User();
     }
 
     /*
@@ -22,5 +28,53 @@ class StudentController extends BaseController
     {
         $data = $this->service->getAvailableSchedule($id,$event_id);
         return $this->success($data);
+    }
+
+    public function bulkInsert(Request $request)
+    {
+        $data = $request->data;
+
+        foreach ($data as $d){
+            // validator
+            $valid = Validator::make($d,[
+                'email' => 'required|email',
+                'name' => 'required'
+            ],[
+                'email.required' => 'Email is required',
+                'email.email' => 'Email is not valid',
+                'name.required' => 'Name is required'
+            ]);
+            if($valid->fails()){
+                return $this->error($valid->errors(),400);
+            }
+            // cek if user exist
+            if($this->user->where('email',$d['email'])->get()->count() == 0){
+                $user = User::create(['email' => $d['email'],'name' => $d['name']]);
+                UserRole::create(['user_id'=>$user->id,'role_id'=>Role::where('slug','user')->first()->id]);
+            }else{
+                $user = User::where('email',$d['email'])->first();
+            }
+            // cek if student exist
+            if($this->model->where('user_id',$user->id)->get()->count() == 0){
+                $this->service->create([
+                    'user_id'=>$user->id,
+                    'ips'=>$d['ips'],
+                    'ipk' => $d['ipk'],
+                    'prs' => json_encode($d['prs']),
+                    'semester' => $d['semester'],
+                    'program' => $d['program']
+                ]);
+            }else{
+                $this->update($user->id,[
+                    'ips'=>$d['ips'],
+                    'ipk' => $d['ipk'],
+                    'prs' => json_encode($d['prs']),
+                    'semester' => $d['semester'],
+                    'program' => $d['program']
+                ]);
+            }
+            
+        }
+        return $this->success('Successfully bulk inserted',200);
     }
 }
