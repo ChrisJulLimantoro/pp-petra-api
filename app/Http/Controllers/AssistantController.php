805e5a1f63fb11091ffa9828253904ba3f8641ee
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2023-11-04 22:50:35 +0700

    fix: add user & student

--- app/Http/Controllers/AssistantController.php
+++ app/Http/Controllers/AssistantController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\BaseController;
 use App\Models\Assistant;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\UserRole;
+use App\Utils\HttpResponseCode;
+use Illuminate\Http\Request;
 
 class AssistantController extends BaseController
 {
+    private $user;
     public function __construct(Assistant $model)
     {
         parent::__construct($model);
+        $this->user = new User();
     }
 
     /*
@@ -17,4 +24,31 @@ class AssistantController extends BaseController
         OR
         Override existing controller here...
     */
+
+    public function store(Request $request)
+    {
+        $data = $request->only(['email','name','room_id','description']);
+
+        // cek if user exist
+        if($this->user->where('email',$data['email'])->get()->count() == 0){
+            $user = User::create(['email' => $data['email'],'name' => $data['name']]);
+            UserRole::create(['user_id'=>$user->id,'role_id'=>Role::where('slug','asdos')->first()->id]);
+        }else{
+            $user = User::where('email',$data['email'])->first();
+            // cek if role exist
+            if(UserRole::where('user_id',$user->id)->where('role_id',Role::where('slug','asdos')->first()->id)->get()->count() == 0){
+                UserRole::create(['user_id'=>$user->id,'role_id'=>Role::where('slug','asdos')->first()->id]);
+            }
+        }
+
+        $data['user_id'] = $user->id;
+        ControllerUtils::validateRequest($this->model, $data);
+
+        $res = $this->service->create($data);
+
+        return $this->success(
+            $res,
+            HttpResponseCode::HTTP_CREATED
+        );
+    }
 }
