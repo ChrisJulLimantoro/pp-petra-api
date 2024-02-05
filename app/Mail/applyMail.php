1b84f775ad43584b2efc7fb0fd04c6be669d2b50
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2024-02-05 09:33:17 +0700

    feat: add testing

--- app/Mail/applyMail.php
+++ app/Mail/applyMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class applyMail extends Mailable implements ShouldQueue
+class applyMail extends Mailable
 {
     use Queueable, SerializesModels;
 
